dfcd7def346fb3227e5ffa5f1121e8ed6ddf4d1b
bk-git-hub <bkprivate99@gmail.com>
2025-09-13 22:03:17 +0900

    chore: add prettier configuration for code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -23,4 +24,5 @@ export default tseslint.config([
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
+  prettierConfig,
 ]);
