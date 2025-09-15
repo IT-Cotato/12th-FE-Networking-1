f591904efcd414a959cfd144786da07edbe82b51
bk-git-hub <bkprivate99@gmail.com>
2025-09-15 15:25:32 +0900

    refactor: separate msw initialize logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import { useState, useEffect } from "react";
 import Header from "@/components/header";
 import MovieForm from "@/components/movie-form";
 import MovieList from "@/components/movie-list";
-import "./index.css";
 
 function App() {
-  const [isWorkerReady, setIsWorkerReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      if (import.meta.env.DEV) {
-        const { worker } = await import("./mocks/browser");
-        await worker.start();
-      }
-      setIsWorkerReady(true);
-    }
-    prepare();
-  }, []);
-
-  if (!isWorkerReady) {
-    return <div>Preparing mock server...</div>;
-  }
-
   return (
     <div className="bg-light-background dark:bg-dark-background text-black dark:text-white min-h-screen p-5 transition-all duration-200 ease-in-out">
       <Header />
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
+// src/main.tsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { enableMocking } from "./mocks";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+});
