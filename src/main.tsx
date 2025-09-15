139f8ad3ff6315d9c3d67dd14d786f76a43eafa9
bk-git-hub <bkprivate99@gmail.com>
2025-09-15 14:09:20 +0900

    refactor(app): move MSW initialization into App component

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser");
-  await worker.start();
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
