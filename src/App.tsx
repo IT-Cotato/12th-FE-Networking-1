d2fbdeefdef3e422f4c93ebd865d157d0db05364
bk-git-hub <bkprivate99@gmail.com>
2025-09-15 14:00:01 +0900

    chore: remove unused codes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useState, useEffect, useMemo } from "react";
-import { themes } from "./styles/theme";
 import "./index.css";
 
-import Input from "@/components/ui/input";
 import Header from "@/components/header";
 import MovieForm from "./components/movie-form";
 import MovieList from "./components/movie-list";
-interface Movie {
-  id: number;
-  title: string;
-  director: string;
-  year: number;
-  genre: string;
-  rating: number;
-}
 
 function App() {
-  const currentTheme = themes["light"];
-
   return (
     <div
       className="bg-light-background dark:bg-dark-background 
