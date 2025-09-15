dd27400bbcfceed6a4d58de7791ec43652480290
bk-git-hub <bkprivate99@gmail.com>
2025-09-15 13:39:33 +0900

    feat: install tanstack query and setup provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
@@ -9,6 +12,8 @@ if (import.meta.env.DEV) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
