2d87037f827be03bcd43aecd23eac60f7cb495a4
kimminna <kmina121777@naver.com>
2025-09-15 20:06:36 +0900

    ✨ feat: QueryClientProvider 초기 세팅 코드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   await worker.start();
 }
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
