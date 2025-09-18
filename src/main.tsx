e5ee7996720001081f4e6432e5cfbc26597150d2
Seoje1405 <hipop99@naver.com>
2025-09-18 21:04:06 +0900

    style : 다크모드 margin 문제 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+import "./index.css";
+
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
