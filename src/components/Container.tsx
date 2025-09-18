353923fc03e62c9041b56ed8a6cbc7d940c97cbc
ion <delion.dev@gmail.com>
2025-09-18 16:31:02 +0900

    feat: 반복되는 사각 컴포넌트를 Container 컴포넌트로 분리

---
+++ src/components/Container.tsx
@@ -0,0 +1,27 @@
+import type { ReactNode } from "react";
+import { useTheme } from "../hooks/useTheme";
+
+interface ContainerProps {
+  title?: string;
+  children: ReactNode;
+}
+
+const Container = ({ title, children }: ContainerProps) => {
+  const { currentTheme } = useTheme();
+
+  return (
+    <div
+      style={{
+        padding: "20px",
+        borderRadius: "12px",
+        backgroundColor: currentTheme.componentBg,
+        border: `1px solid ${currentTheme.border}`,
+      }}
+    >
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
